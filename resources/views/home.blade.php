27146234024c69edaf54a63fe2482c71f977e44d
Roberto Larivera <robylariv@gmail.com>
2023-03-10 00:12:37 +0100

    Inizio main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,27 @@
 
 @section('body_main') {{-- segnaposto dentro body  --}}
     <main>
-        <h1>Hello Word!</h1>
+        <div class="main_jumbotron">
+        </div>
+        <div class="main_current__series">
+            <div>
+                <div class="container">
+                    <div class="title-section">
+                        <h2>current series</h2>
+                    </div>
+                    <div class="series_cards">
+                        {{-- <AppMainCurrentCard :src="element.thumb" :title="element.series" v-for="(element, index) in seriesData" @click="openInfo(index)" /> --}}
+                    </div>
+                    <button>load more</button>
+                </div>
+            </div>
+            {{-- <div>
+                <div class="container">
+                    <AppMainCardInfo :src="seriesData[currentBook].thumb" :title="seriesData[currentBook].series"
+                        :price="seriesData[currentBook].price" :type="seriesData[currentBook].type" />
+                </div>
+    
+            </div> --}}
+        </div>
     </main>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,203 @@
 <footer>
     <div class="footer_top">
         <div class="container">
-            {{-- <AppFooterButtonLinks/> --}}
+            <div class="content_links">
+                <ul>
+                    <li>
+                        <a href="#nogo">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt=""> <!--target="_blank"-->
+                            <span>digital comics</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#nogo">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                            <span>dc merchandise</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#nogo">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                            <span>subscription</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#nogo">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                            <span>comic shop locator</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#nogo">
+                            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                            <span>dc power visa</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
             
         </div>
     </div>
     <div class="footer_center multi-bg">
         <div class="container">
-            {{-- <AppFooterListLinks/> --}}
+            <div class="center_content">
+                <div class="center_content-links">
+                    <div class="content_links-left">
+                        <h3>
+                            dc comics
+                        </h3>
+                        <ul>
+                            <li>
+                                <a href="#nogo">
+                                    Characters
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Comics
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Movies
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    TV
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Games
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Videos
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    News
+                                </a>
+                            </li>
+                        </ul>
+                        <h3>
+                            shop
+                        </h3>
+                        <ul>
+                            <li>
+                                <a href="#nogo">
+                                    Shop DC
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Shop DC Collectibles
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="content_links-center">
+                        <h3>
+                            dc
+                        </h3>
+                        <ul>
+                            <li>
+                                <a href="#nogo">
+                                    Terms Of Use
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Privacy policy (New)
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Ad Choices
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Advertising
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Jobs
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Subscriptions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Talent Workshops
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    CPSC Certificates
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Ratings
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Shop Help
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    Contact Us
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="content_links-right">
+                        <h3>
+                            sites
+                        </h3>
+                        <ul>
+                            <li>
+                                <a href="#nogo">
+                                    DC
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    MAD Magazine
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    DC Kids
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    DC Universe
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#nogo">
+                                    DC Power Visa
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="center_content-logo">
+                    </div>
+        
+                </div>
+            </div>
         </div>
     </div>
     <div class="footer_bottom">
@@ -24,27 +214,27 @@
                         </li>
                         <li>
                             <a href="#nogo">
-                                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="icon-facebook">
+                                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}">
                             </a>
                         </li>
                         <li>
                             <a href="#nogo">
-                                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="icon-facebook">
+                                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}">
                             </a>
                         </li>
                         <li>
                             <a href="#nogo">
-                                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="icon-facebook">
+                                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}">
                             </a>
                         </li>
                         <li>
                             <a href="#nogo">
-                                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="icon-facebook">
+                                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}">
                             </a>
                         </li>
                         <li>
                             <a href="#nogo">
-                                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="icon-facebook">
+                                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}">
                             </a>
                         </li>
                     </ul>
