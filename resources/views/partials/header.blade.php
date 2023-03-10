6fabacf1afbe466cb623b579e224831e7c2411a6
Roberto Larivera <robylariv@gmail.com>
2023-03-10 01:30:27 +0100

    mod header + search

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,48 +1,67 @@
 <header>
     <div class="container">
-        <div class="header_container">
-            <div class="header_logo">
-                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}">
+        <div class="row header_container">
+
+            <div class="col h-100">
+                <div class="header_logo">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}">
+                </div>
             </div>
-            <div class="header_links">
-                    
-                        <ul>
-                            {{-- qui i link  --}}
-                            <li>
-                                <a href="#nogo">characters</a>
-                            </li>
-                            <li>
-                                <a href="#nogo">comics</a>
-                            </li>
-                            <li>
-                                <a href="#nogo">movies</a>
-                            </li>
-                            <li>
-                                <a href="#nogo">tv</a>
-                            </li>
-                            <li>
-                                <a href="#nogo">games</a>
-                            </li>
-                            <li>
-                                <a href="#nogo">collectibles</a>
-                                </li>
-                            <li>
-                                <a href="#nogo">videos</a>
-                                </li>
-                            <li>
-                                <a href="#nogo">fans</a>
-                                </li>
-                            <li>
-                                <a href="#nogo">news</a>
-                                </li>
-                            <li>
-                                <a href="#nogo">shop</a>
-                                </li>
-                        </ul>
-                        
-                    
+            <div class="col h-100">
+                <div class="header_links ">
+                    <ul class="d-flex">
+                        {{-- qui i link  --}}
+                        <li>
+                            <a href="#nogo">characters</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">comics</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">movies</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">tv</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">games</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">collectibles</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">videos</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">fans</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">news</a>
+                        </li>
+                        <li>
+                            <a href="#nogo">shop</a>
+                        </li>
+                    </ul>
+
+
+                </div>
             </div>
+            <div class="col h-100 d-flex align-items-center">
+                <div class="input-group">
+                    <div class="form-floating">
+                        <input type="search" class="form-control" id="###" placeholder="#">
+                        <label for="###">Search</label>
+                    </div>
+                    <span class="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></span>
+                </div>
+            </div>
+
+
+
         </div>
+
+
+    </div>
 
     </div>
 </header>
